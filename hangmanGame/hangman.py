8b8039b238d027ac19b6bd1082f4464eecf2b228
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2022-12-15 21:51:35 +0100

    hangman

---
+++ hangmanGame/hangman.py
@@ -0,0 +1,57 @@
+#HANGMAN GAME WITH PYTHON
+
+import random
+import hangman_words
+
+chosen_word = random.choice(hangman_words.word_list)
+word_length = len(chosen_word)
+
+end_of_game = False
+lives = 6
+
+
+import hangman_art
+print(hangman_art.logo)
+
+#Testing code
+print('Pssst, the solution is Unknown.')
+
+#Create blanks
+display = []
+for _ in range(word_length):
+    display += "_"
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+
+    
+    if guess == display:
+      print(f"The letter guessed is {guess}")
+
+    #Check guessed letter
+    for position in range(word_length):
+        letter = chosen_word[position]
+        # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        if letter == guess:
+            display[position] = letter
+
+    #Check if user is wrong.
+    if guess not in chosen_word:
+        
+        print(f"The letter guessed is {guess}, but its not in the word, so you lost a life")
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
+
+    #Join all the elements in the list and turn it into a String.
+    print(f"{' '.join(display)}")
+
+    #Check if user has got all letters.
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+   
+    from hangman_art import stages
+    print(stages[lives])
