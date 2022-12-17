c6fcbb0cf9464a5e874a3c6c736272753935b6fb
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2022-12-17 17:40:17 +0100

    cypher encryption

---
+++ ceaser cypher encryption/main.py
@@ -0,0 +1,38 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+
+#GAME PROG FUNCTION
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  if cipher_direction == "decode":
+    shift_amount *= -1
+  for char in start_text:
+    if char in alphabet:
+      position = alphabet.index(char)
+      new_position = position + shift_amount
+      end_text += alphabet[new_position]
+    else:
+      end_text += char 
+    
+  print(f"Here's the {cipher_direction}d result: {end_text}")
+
+
+
+####################################################
+
+#GAME PROGRAM CL
+
+from art import logo
+print(logo)
+
+con_game = True
+while con_game:
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+  shift = shift % 26
+  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+  req = input("Do you want to play again, yes or no: ")
+  if req == "no":
+    con_game = False
+    print("Goodbye.")
