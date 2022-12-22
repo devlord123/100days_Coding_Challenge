a5683919ff1852181370b962932c1608f43b0bfe
Owoyemi Idris Olamilekan <106987409+Halfboyfriend@users.noreply.github.com>
2022-12-22 11:44:48 +0100

    Update leap-year.py

--- leap-year.py
+++ leap-year.py
@@ -13,3 +13,38 @@ if year % 4 == 0:
         print("Leap year.")
 else:
     print("Not leap year.")
+
+    
+    ###############################################
+    """Leap year calculations with functions"""
+def is_leap(year):
+  if year % 4 == 0:
+    if year % 100 == 0:
+      if year % 400 == 0:
+        return True
+      else:
+        return False
+    else:
+      return True
+  else:
+    return False
+
+def days_in_month(y, m):
+  if m > 12 or m < 1:
+      return "Invalid inputs"  
+  month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+  if is_leap(y) == False:
+    new = month_days[m - 1]
+    return new
+  elif is_leap(y) == True:
+    month_days[2] = 29
+    new = month_days[m - 1]
+    return new 
+
+
+
+year = int(input("Enter a year: "))
+month = int(input("Enter a month: "))
+days = days_in_month(year, month)
+print(days)
