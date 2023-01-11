a3fec8a693b92f3ba7b0c5fc9bd9fa574ab17c70
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-01-11 17:20:45 +0100

    task

---
+++ OOP_python/question_model.py
@@ -0,0 +1,11 @@
+class Question:
+
+    def __init__(self, q_text, q_answer):
+        self.text = q_text
+        self.answer = q_answer
+
+class User:
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
