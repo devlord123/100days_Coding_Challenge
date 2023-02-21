13343abebce0f072a7b10d07a0bda65a55fc10a5
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-02-21 21:35:05 +0100

    all

--- Password_manager_tkinter(GUI)/code.py
+++ Password_manager_tkinter(GUI)/code.py
@@ -1,12 +1,10 @@
-import pyperclip
+
 from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
-<<<<<<< HEAD
+
 import json
-=======
-import pyperclip
->>>>>>> 2b4dac14c3e3bfa88559869461d43e42e62d0a16
+
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -27,7 +25,7 @@ def generate_password():
     shuffle(password_list)
     gen_pas = "".join(password_list)
     password.insert(0, gen_pas)
-    pyperclip.copy(gen_pas)
+    #pyperclip.copy(gen_pas)
     
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
